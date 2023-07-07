f47509dd5676d9d5506107e50325f40926bd4e78
EvaGraceSmith <smithevagrace@gmail.com>
2023-07-07 01:17:15 -0500

    added cards with images

--- src/Components/Products/index.jsx
+++ src/Components/Products/index.jsx
@@ -1,22 +1,47 @@
 import { useSelector } from 'react-redux';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-const Products = () => {
-  const selectedCategory = useSelector((state) => state.categories.activeCategory);
-  const products = useSelector((state) =>
-    state.products.filter((product) => 
-        product.category === selectedCategory),
-  );
 
-  return (
-    <div>
-      <h2>Products</h2>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>{product.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
+
+export default function Products() {
+    const selectedCategory = useSelector((state) => state.categories.activeCategory);
+    const products = useSelector((state) =>
+        state.products.filter((product) =>
+            product.category === selectedCategory),
+    );
+
+    return (
+        <div>
+            <h2>Products</h2>
+            {products.map((product) => (
+                <Card key={product.id}
+                sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                        sx={{ height: 140 }}
+                        image={product.url}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {product.name}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {product.description}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">ADD TO CART</Button>
+                        <Button size="small">VIEW DETAILS</Button>
+                    </CardActions>
+                </Card>
+            ))}
+
+        </div>
+    );
 };
 
-export default Products;
+
--- src/store/products.js
+++ src/store/products.js
@@ -1,11 +1,11 @@
 const initialState = [
-    { id:1, name: 'TV', category: 'electronics', price: 699.00, inStock: 5 },
-    { id:2, name: 'Radio', category: 'electronics', price: 99.00, inStock: 15 },
-    { id:3, name: 'Shirt', category: 'clothing', price: 9.00, inStock: 25 },
-    { id:4, name: 'Socks', category: 'clothing', price: 12.00, inStock: 10 },
-    { id:5, name: 'Apples', category: 'food', price: .99, inStock: 500 },
-    { id:6, name: 'Eggs', category: 'food', price: 1.99, inStock: 12 },
-    { id:7, name: 'Bread', category: 'food', price: 2.39, inStock: 90 },
+    { id:1, name: 'TV', category: 'electronics', price: 699.00, inStock: 5, description: 'A panel with pixels', url: 'https://source.unsplash.com/random?tv' },
+    { id:2, name: 'Radio', category: 'electronics', price: 99.00, inStock: 15, description: 'Listen to the news', url: 'https://source.unsplash.com/random?radio' },
+    { id:3, name: 'Shirt', category: 'clothing', price: 9.00, inStock: 25, description: 'A nice blue shirt', url: 'https://source.unsplash.com/random?shirt' },
+    { id:4, name: 'Socks', category: 'clothing', price: 12.00, inStock: 10, description: 'A pair of socks', url: 'https://source.unsplash.com/random?socks' },
+    { id:5, name: 'Apples', category: 'food', price: .99, inStock: 500, description: 'A bag of 10 apples', url: 'https://source.unsplash.com/random?apples' },
+    { id:6, name: 'Eggs', category: 'food', price: 1.99, inStock: 12, description: 'A dozen eggs', url: 'https://source.unsplash.com/random?eggs' },
+    { id:7, name: 'Bread', category: 'food', price: 2.39, inStock: 90, description: 'A loaf of bread', url: 'https://source.unsplash.com/random?bread' },
   ];
   
   const productsReducer = (state = initialState, action) => {
