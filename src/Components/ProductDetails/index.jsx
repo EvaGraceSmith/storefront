8e308307fc90a9f4554352a7125824a115cb2e3a
EvaGraceSmith <smithevagrace@gmail.com>
2023-07-15 14:21:10 -0500

    updated productdetails

--- src/Components/ProductDetails/index.jsx
+++ src/Components/ProductDetails/index.jsx
@@ -1,6 +1,56 @@
 
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material';
+import { ,addDispatcher } from '../../store/cart/cart';
+import { addToCart } from '../../store/cart/cart';  
+import { decrementInventoryOnAdd, } from '../../store/products';
+
+
+
+
 export default function ProductDetails() {
+  const products = useSelector(state => state.products);
+  const { id } = useParams();
+  const selectedProduct = products.find(product => product._id === id);
+  console.log('selectedProduct', selectedProduct);
+  const dispatch = useDispatch();
+
+  const addDispatcher = (product) => {
+    dispatch(addToCart(product));
+    dispatch(decrementInventoryOnAdd(product));
+  };
+
+
   return (
-    <h2>ProductsDetails</h2>
+    <div style= {{margin: "auto", marginTop: '50px'}}>
+      <Card sx ={{ maxWidth: '80%'}}>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div" textAlign="center">
+            {selectedProduct.name}
+          </Typography>
+        </CardContent>
+        <CardMedia
+        sx={{ height: 200 }}
+        image={`https://source.unsplash.com/random?${selectedProduct.name}`}
+        title={selectedProduct.name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {selectedProduct.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {selectedProduct.description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button 
+          onClick={() => addDispatcher(selectedProduct)}
+          size="small">ADD TO CART</Button>
+          <Button size="small">VIEW DETAILS</Button>
+        </CardActions>
+      </Card>
+    </div>
+
   );
 }
--- src/Components/ShoppingCart/index.jsx
+++ src/Components/ShoppingCart/index.jsx
@@ -8,6 +8,8 @@ export default function ShoppingCart() {
   const cart  = useSelector(state => state.cart);
   return (
     <Box sx={{ flexGrow: 1 }}>
+      <h2>Shopping Cart</h2>
+      <h3>Items in Cart: {cart.length}</h3>
           <Button color="inherit"
           component={Link}
         to={`/}`}
